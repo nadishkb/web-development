ebeabcdb69ef1b4f227005dc6cd697041b0f25b0
Nadish <nadishkb@gmail.com>
2023-12-19 08:16:24 +0530

    start my web project

---
+++ script.js
@@ -0,0 +1,14 @@
+let country ='india';
+let continent='asia';
+let population = 1.4;
+console.log(country+"-"+continent+"-"+population);
+
+let isIsland = true;
+const  language = 11110;
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof language);
+
+language = 'tamil';
+
